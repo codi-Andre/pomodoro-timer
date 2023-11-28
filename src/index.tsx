410344da1ce24ff6ed21905b22c23da03e39af6c
André Miranda <aegis.andre@gmail.com>
2023-11-28 11:18:08 -0300

    styles: add theme colors

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
 /* @refresh reload */
-import { render } from 'solid-js/web'
+import { render } from "solid-js/web"
 
-import './index.css'
-import App from './App'
+import "./reset-styles.css"
+import "./index.css"
+import App from "./App"
 
-const root = document.getElementById('root')
+const root = document.getElementById("root")
 
 render(() => <App />, root!)

c98f8db896f0eb64c4d7acef645aa51d5f2a2bb0
André Miranda <aegis.andre@gmail.com>
2023-12-04 08:32:52 -0300

    feat: add timer context

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
+import { Show, createSignal } from "solid-js"
 import styles from "./app.module.css"
-import { Timer } from "./components/timer/timer"
 import { Display } from "./components/display/display"
+import { Menu } from "./components/menu/menu"
 import { Panel } from "./components/panel/panel"
+import { TimerProvider } from "./contexts/timer-context"
 
 function App() {
+  const [isMenuOpen, setIsMenuOpen] = createSignal(false)
+
   return (
-    <main class={styles.card}>
-      <header>
-        <button class={styles.menu} aria-label="menu">
+    <TimerProvider>
+      <main class={styles.card}>
+        <button
+          class={styles["menu-btn"]}
+          aria-label="menu"
+          onClick={() => setIsMenuOpen((prev) => !prev)}
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="24"
@@ -18,20 +26,22 @@ function App() {
             stroke-width="2"
             stroke-linecap="round"
             stroke-linejoin="round"
+            class={styles["menu-svg"]}
           >
             <line x1="4" x2="20" y1="12" y2="12" />
             <line x1="4" x2="20" y1="6" y2="6" />
             <line x1="4" x2="20" y1="18" y2="18" />
           </svg>
         </button>
-      </header>
+        <Show when={isMenuOpen()}>
+          <Menu closeMenu={setIsMenuOpen} />
+        </Show>
 
-      <Timer />
+        <Display />
 
-      <Display />
-
-      <Panel />
-    </main>
+        <Panel />
+      </main>
+    </TimerProvider>
   )
 }
 
---
+++ src/contexts/timer-context.tsx
@@ -0,0 +1,197 @@
+import { JSX, createContext, onCleanup, useContext } from "solid-js"
+import { Timer } from "../utils/timer"
+import { createStore } from "solid-js/store"
+import { formatTimeToTwoDigits } from "../utils/formatTimeToTwoDigits"
+import { convertToPercentage } from "../utils/convertToPercentage"
+
+interface TimerState {
+  time: string
+  timeWork: number
+  timeRest: number
+  timeInPercentage: number
+  countDown: boolean
+  isRunning: boolean
+  timerId: NodeJS.Timeout | undefined
+}
+
+interface TimerMethods {
+  start: () => void
+  stop: () => void
+  reset: () => void
+  clear: () => void
+  countMode: () => void
+  countdownMode: () => void
+  setTimeInMin: (timeWork?: number, timeRest?: number) => void
+}
+
+type TimerStore = [TimerState, TimerMethods]
+
+const initialState: TimerState = {
+  time: "25:00",
+  timeWork: 25,
+  timeRest: 5,
+  timeInPercentage: 0,
+  countDown: true,
+  isRunning: false,
+  timerId: undefined
+}
+
+const TimerContext = createContext([initialState, {}] as TimerStore)
+
+interface TimerProviderProps {
+  children: JSX.Element
+}
+
+export function TimerProvider(props: TimerProviderProps) {
+  const [state, setState] = createStore(initialState)
+
+  const timer = new Timer()
+
+  onCleanup(() => clearInterval(state.timerId))
+
+  function updateCountTime() {
+    const timeInPercentage = convertToPercentage(
+      timer.getTimeRemaining,
+      timer.targetTime
+    )
+    let time: number[] | string
+
+    time = [timer.getTimeInMin, timer.getTimeInSec]
+    time = formatTimeToTwoDigits(time)
+
+    setState({ time, timeInPercentage })
+
+    if (timeInPercentage <= 0) {
+      switchTimerMode()
+    }
+  }
+
+  function updateCountdownTime() {
+    const timeInPercentage = convertToPercentage(
+      timer.getTime,
+      timer.targetTime
+    )
+    let time: number[] | string
+
+    time = [timer.getTimeRemainingInMin, timer.getTimeRemainingInSec]
+    time = formatTimeToTwoDigits(time)
+
+    setState({ time, timeInPercentage })
+
+    if (timeInPercentage >= 100) {
+      switchTimerMode()
+    }
+  }
+
+  function setTargetTime(minutes: number = 25) {
+    const oneMinuteInMs = 1000 * 60
+
+    timer.setTargetTime = oneMinuteInMs * minutes
+  }
+
+  function start() {
+    timer.start()
+    setState("isRunning", true)
+
+    if (state.countDown) {
+      setState("timerId", setInterval(updateCountdownTime, 1000))
+    } else {
+      setState("timerId", setInterval(updateCountTime, 1000))
+    }
+  }
+
+  function stop() {
+    timer.stop()
+
+    setState("isRunning", false)
+    clearInterval(state.timerId)
+  }
+
+  function reset() {
+    timer.reset()
+
+    if (state.countDown) {
+      setState("time", formatTimeToTwoDigits([state.timeWork, 0]))
+    } else {
+      setState("time", formatTimeToTwoDigits([0, 0]))
+    }
+  }
+
+  function clear() {
+    timer.clear()
+
+    clearInterval(state.timerId)
+    setState({
+      time: "25:00",
+      countDown: true,
+      timeWork: 25,
+      timeRest: 5,
+      isRunning: false
+    })
+  }
+
+  function countMode() {
+    clearInterval(state.timerId)
+
+    timer.clear()
+
+    setState({ time: "00:00", countDown: false, isRunning: false })
+  }
+
+  function countdownMode() {
+    clearInterval(state.timerId)
+
+    timer.clear()
+  }
+
+  function switchTimerMode() {
+    clearInterval(state.timerId)
+    timer.clear()
+
+    if (state.countDown) {
+      setTargetTime(state.timeRest)
+      setState({
+        time: "00:00",
+        countDown: false,
+        isRunning: false,
+        timeInPercentage: 100
+      })
+    } else {
+      setTargetTime(state.timeWork)
+      setState({
+        time: formatTimeToTwoDigits([state.timeWork, 0]),
+        countDown: true,
+        isRunning: false,
+        timeInPercentage: 0
+      })
+    }
+  }
+
+  function setTimeInMin(timeWork: number = 25, timeRest: number = 5) {
+    timer.clear()
+    clearInterval(state.timerId)
+
+    setState({ timeWork, timeRest, isRunning: false })
+
+    if (state.countDown) {
+      setTargetTime(state.timeWork)
+      setState("time", formatTimeToTwoDigits([state.timeWork, 0]))
+    } else {
+      setTargetTime(state.timeRest)
+      setState("time", formatTimeToTwoDigits([state.timeRest, 0]))
+    }
+  }
+
+  return (
+    <TimerContext.Provider
+      value={[
+        state,
+        { clear, countdownMode, countMode, reset, setTimeInMin, start, stop }
+      ]}
+    >
+      {props.children}
+    </TimerContext.Provider>
+  )
+}
+
+export const useTimerContext = () => useContext(TimerContext)

df3c9ace4676d53d70079a8bfadd492c9ae79eb6
André Miranda <aegis.andre@gmail.com>
2023-12-06 12:48:34 -0300

    build: config vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
+/// <reference types="vitest" />
+
 import { defineConfig } from "vite"
 import solid from "vite-plugin-solid"
 
 export default defineConfig({
   plugins: [solid()],
-  base: "/pomodoro-timer"
+  base: "/pomodoro-timer",
+  test: {
+    environment: "jsdom",
+    globals: true,
+    // prettier-ignore
+    testTransformMode: { web: ["/\.[jt]sx?$/"] }, // eslint-disable-line no-useless-escape
+    coverage: {
+      provider: "v8",
+      enabled: true,
+      reporter: ["html"]
+    }
+  }
 })
